8860110e0da652f92f26fbb03c908b2498144fe1
Augustine Smith <augustine.smith@amalitech.org>
2022-11-17 12:07:17 +0000

    Rust smart bookmarking

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rust smart Bookmarking System
+
+### easy way to search on Twitter and Github
+
+## Commands
+
+#### Search for a twitter profile
+
+```
+tw @codewithflash
+```
+
+#### Search for a github profile
+
+```
+gh The-Flash
+```
+
+Fallback to Google if nothing matches
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate rocket;
+
+use rocket::response::Redirect;
+
+mod utils;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/search?<cmd>")]
+fn search(cmd: String) -> Redirect {
+    let command = utils::get_command_from_query_string(&cmd);
+    let redirect_url = match command {
+        "gh" => utils::github::construct_github_url(&cmd),
+        "tw" => utils::twitter::construct_twitter_url(&cmd),
+        _ => utils::google::construct_google_search_from_url(&cmd)
+    };
+    Redirect::to(redirect_url)
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, search])
+}
